7294caba59d2949e1544ef023829d4050721624a
Thimaphon Kan <49416670+timkan12@users.noreply.github.com>
2019-04-20 16:14:55 -0600

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
+
 void displayMenu()
 {
   cout << "\t\t\t\t\tHello! Welcome to Kathow Rental" << endl;
@@ -20,8 +23,9 @@ void rentMenu()
   cout << "\t\t\t\t\t1. Username:" << endl;
   cout << "\t\t\t\t\t2. Car Model:" << endl;
   cout << "\t\t\t\t\t3. Car Year:" << endl;
-  cout << "\t\t\t\t\t4. Back to Main Menu" << endl;
-  cout << "\t\t\t\t\t5. Quit" << endl;
+  cout << "\t\t\t\t\t4. Go to Check out " << endl;
+  cout << "\t\t\t\t\t5. Back to Main Menu" << endl;
+  cout << "\t\t\t\t\t6. Quit" << endl;
 }
 
 void returnMenu()
@@ -33,6 +37,8 @@ void returnMenu()
   cout << "\t\t\t\t\t4. Back to Main Menu" << endl;
   cout << "\t\t\t\t\t5. Quit" << endl;
 }
+
+
 
 
 int main()
@@ -52,28 +58,74 @@ int main()
         rentMenu();
         cin>> choice2;
         cin.ignore();
+        string name;
+        string type;
+        string year;
         switch(choice2)
         {
           case 1:
-
+          cout << "Please enter your name: " << endl;
+          //use is unique function
+          getline(cin, name);
+          cout << "You entered: " ;
+          for(int i = 0; i < name.size(); i++)
+          {
+          cout << name[i];
+          }
           break;
 
           case 2:
-
+          cout << "Please enter your desire car model:" << endl;
+          getline(cin, type);
+          cout << "Searching for the desired brand: " ;
+          for(int i = 0; i < type.size(); i++)
+          {
+          cout << type[i];
+          }
+          // look for the car
           break;
 
           case 3:
-
+          cout << "Please enter your desire car year:" << endl;
+          cin >> year;
+          cout << "Searching for the desired year: " ;
+          for(int i = 0; i < year.size(); i++)
+          {
+          cout << year[i];
+          }
+          // start looking for the car using a funciton
           break;
 
           case 4:
-          displayMenu();
+          // if the model exist and name hasnt check out a car already
+
+          cout << "You, ";
+          for(int i = 0; i < name.size() ; i++)
+          {
+           cout << name[i];
+          }
+         cout << ", have checked out model: ";
+         for(int i = 0; i < type.size(); i++)
+          {
+          cout << type[i];
+          }
+          cout  << "year: " ;
+          for(int i = 0; i < year.size(); i++)
+          {
+          cout << year[i] << endl;
+          }
           break;
 
           case 5:
-          cout << "Goodbye! See you soon!" << endl;
+          displayMenu();
+          break;
 
-      break;
+          case 6:
+          cout << "Goodbye! See you soon!" << endl;
+          exit(0);
+          break;
+
+
     }
   }
 
@@ -104,7 +156,7 @@ int main()
 
           case 5:
           cout << "Goodbye! See you soon!" << endl;
-
+          exit(0);
           break;
         }
       }
