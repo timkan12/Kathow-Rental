68326feeab8f0c93c24f1a077a340032853ad4ec
nguyenbao99 <49416698+nguyenbao99@users.noreply.github.com>
2019-04-22 17:32:14 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,18 +1,17 @@
 #include "menus.hpp"
 #include "helperfunctions.hpp"
 #include <iomanip>
-//#include "car.hpp"
+#include "carhash.cpp"
+
 
 int main()
 {
-  int size = 100;
+  int size = 100; // max number of users in the system
   customer* arrayOfCustomers[size];   // array that holds all the customer objects.
   string name;
-  //int size = 10;         // max size for ammount of customers can be in the system
   int currentSize = loadcustomerinformation(arrayOfCustomers,size);      // how many people are currently in the system
 
-
-  int choice;
+  int choice = 0;
   while(choice != 3)
   {
     loadcustomerinformation(arrayOfCustomers,size);
@@ -26,25 +25,49 @@ int main()
       getline(cin,name);
       int check;
       check = searchForUserInArray(arrayOfCustomers, currentSize, name);
-      if(check == 0) // if the customer isn't in the system
+      if(check == -1) // if the customer isn't in the system
       {
         cout << "You have been added to our database!" << endl;
         customer* newCustomer = new customer;
         newCustomer->name = name;
         newCustomer->carBrand = "";
         newCustomer->year = "";
+        newCustomer->isRented = 0;
         arrayOfCustomers[currentSize] = newCustomer;
+
         currentSize++;  // at this point a new customer should have been added to the array
-        rentCar(newCustomer->name);
+        cout << "Enter name of car you would like to rent." << endl;
+        string brand;
+        getline(cin,brand);
+        cout << "Enter your desire year" << endl;
+        string enteredYear;
+        getline(cin,enteredYear);
+        newCustomer->carBrand = brand;
+        newCustomer->year = enteredYear;
+        newCustomer->isRented = 1;
 
       }
-      if(check > 0) // if they are in the system
+      else if (check != -1 ) // if they are in the system
       {
         if (arrayOfCustomers[check]->carBrand != ""){
           cout << "You are currently renting out a " << arrayOfCustomers[check]->year << " " << arrayOfCustomers[check]->carBrand << endl;
         }
-        // function that rents car
-        rentCar(arrayOfCustomers[check]->name);
+        // rents car
+        else{
+          cout << "Enter name of car you would like to rent." << endl;
+          string brand;
+          getline(cin,brand);
+          cout << "Enter your desire year" << endl;
+          string enteredYear;
+          getline(cin,enteredYear);
+          arrayOfCustomers[check]->carBrand = brand;
+          arrayOfCustomers[check]->year = enteredYear;
+          arrayOfCustomers[check]->isRented = 1;
+          cout << "You have successfully checked a " << arrayOfCustomers[check]->carBrand <<  " " << arrayOfCustomers[check]->year << endl;
+          writeOutAllArrayInformation(arrayOfCustomers, currentSize);
+          exit(0);
+
+        }
 
       break;
 
@@ -54,29 +77,32 @@ int main()
       getline(cin,name);
       int check;
       check = searchForUserInArray(arrayOfCustomers, currentSize, name);
-      if (check == 0) // not found in the system
+      if (check == -1) // not found in the system
       {
         cout << "User not found." << endl;
         // ask them again for a different name
       }
-      else if (check > 0){ // they are in the system, clear the flag in the hashtable of the car, and clear their variable for car
+      else if (check != -1){ // they are in the system, clear the flag in the hashtable of the car, and clear their variable for car
         cout << "returning car..." << endl;
         // clear the private member of the user
         arrayOfCustomers[check]->carBrand = "";
         arrayOfCustomers[check]->year = "";
+        arrayOfCustomers[check]->isRented = 0;
         /* need to set the flag in the hashtable of the object of cars back to 0
           go through the hashtable and find that car if it exits
             if it does change its flag to 0
-            if it doesn't exist then tell the user the car doesn't exists and ask again or exit. 
+            if it doesn't exist then tell the user the car doesn't exists and ask again or exit.
         */
-
+        cout << "You have successfully returned the car!" << endl;
+        cout << "Have a good day!" << endl;
+        writeOutAllArrayInformation(arrayOfCustomers, currentSize);
+        exit(0);
       }
-
-
       break;
 
       case 3:
       cout << "Thank you, hope to see you soon!" << endl;
+      writeOutAllArrayInformation(arrayOfCustomers, currentSize);
       // write the arrayOfCustomers to a text file in the format of the customer structure
       // everytime the program starts the that text file of the user should be read in and new objects
       // should be created and fill the array with what was previously there before.
